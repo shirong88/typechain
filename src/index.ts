2cd39fa2dc85d6a6ac4ab00d9f39df5d7dc28ed2
shirong <sykim2@parkingcloud.co.kr>
2018-09-04 14:38:41 +0900

    소스 추가

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import * as CryptoJS from 'crypto-js';
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(0, '20202020202', '', 'Hello', 123456);
+
+let blockchain: Block[] = [genesisBlock];
+
+console.log(blockchain);
+
+export { };
